9bd7f29e2033a547597699140054658f16f82aa2
sangjinsu <climbplant39@kakao.com>
2023-10-02 14:52:55 +0900

    ✅ add test main

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import typer
 from rich import print
 
+app = typer.Typer()
 
-def main(name: str, lastname: str = "", formal: bool = False):
-    """
-    Say hi to NAME, optionally with a --lastname.
 
-    If --formal is used, say hi very formally.
-    """
-    if formal:
-        print(f"Good day Ms. {name} {lastname}.")
-    else:
-        print(f"Hello {name} {lastname}")
+@app.command()
+def create(username: str):
+    print(f"Creating user: {username}")
+
+
+@app.command()
+def delete(username: str):
+    print(f"Deleting user: {username}")
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    app()
