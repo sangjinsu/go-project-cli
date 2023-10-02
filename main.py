5f0c61a0a23af8306996a4c17843b875afa5282f
sangjinsu <climbplant39@kakao.com>
2023-10-02 14:38:26 +0900

    📦️ add typer and make example

---
+++ main.py
@@ -0,0 +1,18 @@
+import typer
+from rich import print
+
+
+def main(name: str, lastname: str = "", formal: bool = False):
+    """
+    Say hi to NAME, optionally with a --lastname.
+
+    If --formal is used, say hi very formally.
+    """
+    if formal:
+        print(f"Good day Ms. {name} {lastname}.")
+    else:
+        print(f"Hello {name} {lastname}")
+
+
+if __name__ == "__main__":
+    typer.run(main)
