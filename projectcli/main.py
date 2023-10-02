912d1a3c45591db464ff55b4a696309e892822e1
sangjinsu <climbplant39@kakao.com>
2023-10-02 17:09:35 +0900

    📝 add desc

--- projectcli/main.py
+++ projectcli/main.py
@@ -6,11 +6,17 @@ app = typer.Typer()
 
 @app.command()
 def new(package_name: str):
+    """
+    Create a new Pacakge With Controller, Service, Model, Repository Folder
+    """
     utils.new_package(package_name)
 
 
 @app.command()
 def generate(semantic: str, package_name: str):
+    """
+    Generate folders and files
+    """
     utils.generate_file(semantic, package_name)
 
 
